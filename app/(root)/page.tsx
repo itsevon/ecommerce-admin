0b00c7b951565902cd4b841e55c19baf52626426
Evone <yfh910@gmail.com>
2023-08-15 18:39:51 +0900

    Add userbutton

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className="p-4">
-    <Button size='default' variant="outline">Click here!</Button>
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
  
+export default SetupPage;
