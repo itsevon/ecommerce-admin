025e0da959300e162623eb950e59e3d143b92a4e
Evone <yfh910@gmail.com>
2023-08-15 23:51:01 +0900

    Created dashboard routes

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,38 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import prismadb from "@/lib/prismadb";
+
+export default async function DashboardLayout({
+    children,
+    params
+}: {
+    children: React.ReactNode;
+    params: { storeId: string }
+}) {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect('/sign-in');
+    }
+
+    //load the store with userId
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId
+        }
+    });
+
+    if (!store) {
+        redirect('/');
+    }
+
+    //children are pages in routes folder
+    return (
+        <>
+            <div>This will be a Navbar</div>
+            {children}
+        </>
+    )
+}
