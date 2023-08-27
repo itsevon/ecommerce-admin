ae367b1cf1d9bb3c78242124d1221ed0f0787dfb
Evone <yfh910@gmail.com>
2023-08-28 00:06:01 +0900

    Implement save changes onSubmit function

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import * as z from "zod";
+import axios from "axios";
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
 
 import { Heading } from "@/components/ui/heading"
 import { Button } from "@/components/ui/button";
@@ -26,6 +29,9 @@ const formSchema = z.object({
 export const SettingsForm: React.FC<SettingFormProps> = ({
     initialData
 }) => {
+    const params = useParams();
+    const router = useRouter();
+    
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
 
@@ -35,7 +41,16 @@ export const SettingsForm: React.FC<SettingFormProps> = ({
     });
 
     const onSubmit = async (data: SettingsFormValues) => {
-        console.log(data);
+        try {
+            setLoading(true);
+            await axios.patch(`/api/stores/${params.storeId}`, data);
+            router.refresh();
+            toast.success("Store updated.");
+        } catch (error) {
+            toast.error("Something went wrong.");
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
