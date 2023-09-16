1b204e79d44a03c7254803e1026562c1c8c5fdc9
Evone <yfh910@gmail.com>
2023-09-16 16:22:35 +0900

    added delete store function

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -54,12 +54,26 @@ export const SettingsForm: React.FC<SettingFormProps> = ({
         }
     }
 
+    const onDelete = async () => {
+        try {
+            setLoading(true)
+            await axios.delete(`/api/stores/${params.storeId}`)
+            router.refresh();
+            router.push("/")
+            toast.success("Store deleter.")
+        } catch (error) {
+            toast.error("Make sure you removed all products and categories first.")
+        } finally {
+
+        }
+    }
+
     return (
         <>
             <AlertModal 
                 isOpen={open}
                 onClose={() => setOpen(false)}
-                onConfirm={() => {}}
+                onConfirm={onDelete}
                 loading={loading}
             />
             <div className="flex items-center justify-between">
